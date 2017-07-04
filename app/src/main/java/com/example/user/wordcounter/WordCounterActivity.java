bbc8811d9f37b292dc02f749dae3702ea1d41eba
Filipe <granha.carlos@gmail.com>
2017-07-05 00:33:22 +0100

    connected variables to id's

--- app/src/main/java/com/example/user/wordcounter/WordCounterActivity.java
+++ app/src/main/java/com/example/user/wordcounter/WordCounterActivity.java
@@ -3,13 +3,23 @@ package com.example.user.wordcounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class WordCounterActivity extends AppCompatActivity {
+
+    EditText inputText;
+    TextView answerText;
+    Button countButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_counter);
+        inputText = (EditText)findViewById(R.id.text_to_count);
+        answerText = (TextView)findViewById(R.id.count_result);
+        countButton = (Button)findViewById(R.id.count_button);
         Log.d(getClass().toString(), "onCreate called");
     }
 }
